18ab1c132ab0c969c5c50552264c3b5a69a0c715
shakib <mohammedshakib663@gmail.com>
2026-07-09 02:21:29 +0530

    Move Focus Zone to home quick action buttons below Syllabus, bump version to 20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='student-hub-v19';
+const CACHE='student-hub-v20';
 const STATIC=['./student.html','./index.html','./icon-192.png','./icon-512.png','./student-manifest.json','./admin-manifest.json'];
 
 self.addEventListener('install',e=>{
